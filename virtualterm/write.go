ee312e19daf0f325b6a680132121694fbcad92a0
Laurence Morgan <mail@laurencemorgan.co.uk>
2024-01-08 13:07:57 +0000

    move switch out of for loop

--- virtualterm/write.go
+++ virtualterm/write.go
@@ -32,39 +32,41 @@ func (term *Term) printLoop() {
 		//log.Printf("DEBUG: read rune %d [pos: %d:%d] [size: %d:%d]", r, term.curPos.X, term.curPos.Y, term.size.X, term.size.Y)
 
 		term._mutex.Lock()
+		term.readChar(r)
+		term._mutex.Unlock()
+	}
+}
 
-		switch r {
+func (term *Term) readChar(r rune) {
+	switch r {
 
-		case codes.AsciiCtrlG: // bell (7)
-			// TODO: beep
+	case codes.AsciiCtrlG: // bell (7)
+		// TODO: beep
 
-		case codes.AsciiBackspace, codes.IsoBackspace: // (10) / (127)
-			_ = term.csiMoveCursorBackwards(1)
+	case codes.AsciiBackspace, codes.IsoBackspace: // (10) / (127)
+		_ = term.csiMoveCursorBackwards(1)
 
-		case codes.AsciiTab: // \t (11)
-			term.printTab()
+	case codes.AsciiTab: // \t (11)
+		term.printTab()
 
-		case codes.AsciiCtrlJ: // \n (12)
-			if term.csiMoveCursorDownwards(1) > 0 {
-				term.csiScrollUp(1)
-				term.csiMoveCursorDownwards(1)
-			}
-
-		case codes.AsciiCtrlM: // \r (13)
-			term.curPos.X = 0
-
-		case codes.AsciiEscape: // (27)
-			term.parseC1Codes()
-
-		default:
-			if r < 32 {
-				log.Printf("Unexpected ASCII control character (ignored): %d", r)
-				continue
-			}
-			term.writeCell(r)
+	case codes.AsciiCtrlJ: // \n (12)
+		if term.csiMoveCursorDownwards(1) > 0 {
+			term.csiScrollUp(1)
+			term.csiMoveCursorDownwards(1)
 		}
 
-		term._mutex.Unlock()
+	case codes.AsciiCtrlM: // \r (13)
+		term.curPos.X = 0
+
+	case codes.AsciiEscape: // (27)
+		term.parseC1Codes()
+
+	default:
+		if r < 32 {
+			log.Printf("Unexpected ASCII control character (ignored): %d", r)
+			return
+		}
+		term.writeCell(r)
 	}
 }
 
