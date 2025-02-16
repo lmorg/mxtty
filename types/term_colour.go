08121d98744be3d67ce1836de2fd6a35b4a3bb12
Laurence Morgan <mail@laurencemorgan.co.uk>
2025-02-16 19:09:16 +0000

    colour: add support for alpha channel

--- types/term_colour.go
+++ types/term_colour.go
@@ -4,15 +4,17 @@ type Colour struct {
 	Red   byte
 	Green byte
 	Blue  byte
+	Alpha byte
 }
 
-func (c *Colour) Copy() *Colour {
+/*func (c *Colour) Copy() *Colour {
 	return &Colour{
 		Red:   c.Red,
 		Green: c.Green,
 		Blue:  c.Blue,
+		Alpha: c.Alpha,
 	}
-}
+}*/
 
 // RGB32 combines RGB values into a 32-bit integer
 func (c *Colour) RGB24() uint32 {
@@ -20,13 +22,14 @@ func (c *Colour) RGB24() uint32 {
 }
 
 // RGBA32 combines RGBA values into a 32-bit integer
-func (c *Colour) RGBA32(alpha byte) uint32 {
-	return (uint32(c.Red) << 24) | (uint32(c.Green) << 16) | (uint32(c.Blue) << 8) | uint32(alpha)
+func (c *Colour) RGBA32() uint32 {
+	return (uint32(c.Red) << 24) | (uint32(c.Green) << 16) | (uint32(c.Blue) << 8) | uint32(c.Alpha)
 }
-
-const _ALPHA_UINT32 = 255 << 8
 
 // RGBA compatibility with color.Color
 func (c *Colour) RGBA() (uint32, uint32, uint32, uint32) {
-	return uint32(c.Red) << 8, uint32(c.Green) << 8, uint32(c.Blue) << 8, _ALPHA_UINT32
+	if c.Alpha == 0 {
+		c.Alpha = 255
+	}
+	return uint32(c.Red) << 8, uint32(c.Green) << 8, uint32(c.Blue) << 8, uint32(c.Alpha) << 8
 }
